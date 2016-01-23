ba5901081110afb5bf1050a7efff1d8b6cacc1cb
nik-vitaly <nik.vitaly@mail.ru>
2016-01-24 01:10:12 +0300

    activity splash

---
+++ app/src/main/java/net/nikonorov/advancedmessenger/ActivityMain.java
@@ -0,0 +1,18 @@
+package net.nikonorov.advancedmessenger;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vitaly on 24.01.16.
+ */
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/net/nikonorov/advancedmessenger/ActivitySplash.java
+++ app/src/main/java/net/nikonorov/advancedmessenger/ActivitySplash.java
@@ -1,10 +1,53 @@
 package net.nikonorov.advancedmessenger;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 
 /**
  * Created by vitaly on 24.01.16.
  */
 public class ActivitySplash extends Activity {
+
+    private int PAUSE = 2000;
+    private boolean isActive = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isActive = true;
+        new Thread(new Launcher()).start();
+    }
+
+    @Override
+    protected void onStop() {
+        isActive = false;
+        super.onStop();
+    }
+
+    private class Launcher implements Runnable{
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(PAUSE);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if(isActive){
+                startActivity(new Intent(ActivitySplash.this, ActivityMain.class));
+                ActivitySplash.this.finish();
+            }
+
+        }
+    }
+
 
 }
