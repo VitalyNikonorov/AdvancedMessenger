97e4c1ca2466dec8ebdbe01780cf68176c649958
nik-vitaly <nik.vitaly@mail.ru>
2016-01-24 01:45:57 +0300

    signin fragment

--- app/src/main/java/net/nikonorov/advancedmessenger/ActivityMain.java
+++ app/src/main/java/net/nikonorov/advancedmessenger/ActivityMain.java
@@ -1,7 +1,6 @@
 package net.nikonorov.advancedmessenger;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -12,7 +11,6 @@ public class ActivityMain extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
     }
 }
