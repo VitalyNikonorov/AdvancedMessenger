389ebc63bfb7eed8de2c78daa08cf7f9e18c5f88
nik-vitaly <nik.vitaly@mail.ru>
2016-01-25 22:55:38 +0300

    hide keyboard and off history of signin

--- app/src/main/java/net/nikonorov/advancedmessenger/ui/ActivityMain.java
+++ app/src/main/java/net/nikonorov/advancedmessenger/ui/ActivityMain.java
@@ -2,12 +2,16 @@ package net.nikonorov.advancedmessenger.ui;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import net.nikonorov.advancedmessenger.R;
 
@@ -35,6 +39,25 @@ public class ActivityMain extends AppCompatActivity {
         viewPager = (ViewPager) findViewById(R.id.pager);
         pagerAdapter = new MyFragmentPagerAdapter(getFragmentManager());
         viewPager.setAdapter(pagerAdapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.i("TAG", "Scrolled");
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                View view = ActivityMain.this.getCurrentFocus();
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        });
 
         titles[FragmentSet.CHATLIST] = getString(R.string.main_chats);
         titles[FragmentSet.CONTACTS] = getString(R.string.main_contacts);
