4dea13e73fd1e21f814481da8a82a9abea009475
nik-vitaly <nik.vitaly@mail.ru>
2016-01-25 22:38:44 +0300

    view pager done

--- app/src/main/java/net/nikonorov/advancedmessenger/ui/ActivityMain.java
+++ app/src/main/java/net/nikonorov/advancedmessenger/ui/ActivityMain.java
@@ -1,7 +1,12 @@
 package net.nikonorov.advancedmessenger.ui;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import net.nikonorov.advancedmessenger.R;
@@ -11,7 +16,12 @@ import net.nikonorov.advancedmessenger.R;
  */
 public class ActivityMain extends AppCompatActivity {
 
-    private Fragment[] fragments = new Fragment[3];
+    private final int PAGE_COUNT = 3;
+
+    private Fragment[] fragments = new Fragment[PAGE_COUNT];
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+    private String[] titles = new String[PAGE_COUNT];
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,5 +31,35 @@ public class ActivityMain extends AppCompatActivity {
         fragments[FragmentSet.CHATLIST] = new FragmentChatList();
         fragments[FragmentSet.CONTACTS] = new FragmentContacts();
         fragments[FragmentSet.PROFILE] = new FragmentProfile();
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new MyFragmentPagerAdapter(getFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        titles[FragmentSet.CHATLIST] = getString(R.string.main_chats);
+        titles[FragmentSet.CONTACTS] = getString(R.string.main_contacts);
+        titles[FragmentSet.PROFILE] = getString(R.string.main_profile);
+    }
+
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public MyFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
     }
 }
--- app/src/main/java/net/nikonorov/advancedmessenger/ui/FragmentSignin.java
+++ app/src/main/java/net/nikonorov/advancedmessenger/ui/FragmentSignin.java
@@ -1,11 +1,13 @@
 package net.nikonorov.advancedmessenger.ui;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import net.nikonorov.advancedmessenger.R;
 
@@ -27,6 +29,16 @@ public class FragmentSignin extends Fragment {
             @Override
             public void onClick(View v) {
                 ((ActivitySign)getActivity()).changeFragment(FragmentSet.SIGNIN, FragmentSet.SIGNUP);
+            }
+        });
+
+        Button signinButton = (Button)  view.findViewById(R.id.signin_auth_action);
+
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ActivityMain.class);
+                startActivity(intent);
             }
         });
 
